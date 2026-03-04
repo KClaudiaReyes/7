6c673d2ef299dccee6011870f4fef244e991cc95
clauR <creyesccf@gmail.com>
2026-03-04 20:12:25 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary < int , string > productos = new Dictionary < int , string > ();
+
+            int cod = 456;
+            string = " USB"
+            
+
+        }
+    }
+}
