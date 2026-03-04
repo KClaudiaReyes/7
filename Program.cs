d706fe0ab0ec09cd3dd610e2072d14c9870ebd83
clauR <creyesccf@gmail.com>
2026-03-04 20:23:58 +0100

    validar y mostrar

--- Program.cs
+++ Program.cs
@@ -11,9 +11,26 @@ namespace _7
         static void Main(string[] args)
         {
             Dictionary < int , string > productos = new Dictionary < int , string > ();
+            int     ID = 589;
+            Console.WriteLine (" ingrese el codigo del producto: ");
+            int cod = int.Parse(Console.ReadLine());
 
-            int cod = 456;
-            string = " USB"
+            Console.WriteLine("ingrese el nombre del producto: ");
+            string nombre = Console.ReadLine();
+
+            if (!productos.ContainsKey(589))
+            {
+                Console.WriteLine(" ERROR : el codigo "  + " ya esta asignado ");
+            }
+            else
+            {
+                 productos.Add(cod, nombre);
+                Console.WriteLine(" producto " + nombre + " se agrego exitosamente");
+                
+                    
+            }
+
+
             
 
         }
